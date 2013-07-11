33c525e6740328c36796d84c06a1f593892cdab5
Mehrzad Far <mehrzadfar@gmail.com>
2013-07-10 18:34:10 -0700

    Refactoring, adding of the if statements to actually print the numbers

--- num_specs.rb
+++
@@ -1,5 +0,0 @@
-#def tail_scanner
-spec_examples_array = %x( rspec | tail -1 ).scan(/\d+/)
-#end
-saved_current_time = Time.now
-
---
+++ spec_counter.rb
@@ -0,0 +1,26 @@
+def rspec_scanner(file_specified)
+  %x( rspec #{file_specified} ).scan(/\d+\.\d+ second.|\d+ example.|\d+ failure.|\d+ pending/)
+end
+#exec?
+spec_examples_array = rspec_scanner("")
+
+  #end
+saved_current_time = Time.now
+
+#spec_run_time = %x( rspec ).scan(/\d+\.\d+ seconds/)
+puts saved_current_time
+spec_examples_array.each do |case_example|
+  if !case_example.scan(/\d+\.\d+ second./).empty?
+    test_runtime = case_example.scan(/\d+\.\d+/)
+    puts "seconds", test_runtime
+  elsif !case_example.scan(/example./).empty?
+    number_of_examples = case_example.scan(/\d+/)
+    puts "examples", number_of_examples
+  elsif !case_example.scan(/failure./).empty?
+    number_of_failures = case_example.scan(/\d+/)
+    puts "failures", number_of_failures
+  elsif !case_example.scan(/pending/).empty?
+    number_of_pending = case_example.scan(/\d+/)
+    puts "pending", number_of_pending
+  end
+end
