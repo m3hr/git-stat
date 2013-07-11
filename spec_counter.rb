eca51380873dc59340ccb5defb0b5102a9602b58
Mehrzad Far <mehrzadfar@gmail.com>
2013-07-11 16:59:23 -0700

    Module experimentation, wrapped spec counter in module, gitstat file

--- spec_counter.rb
+++ spec_counter.rb
@@ -1,41 +1,41 @@
 require 'csv'
 
-def rspec_scanner(file_specified)
-  %x( rspec #{file_specified} ).scan(/\d+\.\d+ second.|\d+ example.|\d+ failure.|\d+ pending/)
-end
-#exec? edit: no need, string interpolation works in rspec_scanner
-spec_examples_array = rspec_scanner("~/junk/git-stat/spec/fixtures/no_passing_no_failing_some_pending.rb")
-
- saved_current_time = Time.now
-def is_regex_empty?(current_string, regex_match)
+module RspecScanner
+  def rspec_scanner(file_specified)
+    %x( rspec #{file_specified} ).scan(/\d+\.\d+ second.|\d+ example.|\d+ failure.|\d+ pending/)
+  end
+  #exec? edit: no need, string interpolation works in rspec_scanner
+  #spec_examples_array = rspec_scanner("~/junk/git-stat/spec/fixtures/no_passing_no_failing_some_pending.rb")
   
-end
-def title_csv(csv_file_location)
-  CSV.open("#{csv_file_location}","ab") do |csv|
-    csv << ["Run time of tests", "Examples", "Failures", "Pending"]
+  def title_csv(csv_file_location)
+    CSV.open("#{csv_file_location}","ab") do |csv|
+      csv << ["Run time of tests", "Examples", "Failures", "Pending"]
+    end
   end
-end
-#spec_run_time = %x( rspec ).scan(/\d+\.\d+ seconds/)
-puts saved_current_time
-spec_examples_array.each do |case_example|
-  if !case_example.scan(/\d+\.\d+ second./).empty?
-    test_runtime = case_example.scan(/\d+\.\d+/)
-    puts "seconds", test_runtime
-  elsif !case_example.scan(/example./).empty?
-    number_of_examples = case_example.scan(/\d+/)
-    puts "examples", number_of_examples
-  elsif !case_example.scan(/failure./).empty?
-    number_of_failures = case_example.scan(/\d+/)
-    puts "failures", number_of_failures
-  elsif !case_example.scan(/pending/).empty?
-    number_of_pending = case_example.scan(/\d+/)
-    puts "pending", number_of_pending
+  #spec_run_time = %x( rspec ).scan(/\d+\.\d+ seconds/)
+  def rspec_output_finder(current_array)
+    current_array.each do |case_example|
+      if !case_example.scan(/\d+\.\d+ second./).empty?
+        test_runtime = case_example.scan(/\d+\.\d+/)
+        puts "seconds", test_runtime
+      elsif !case_example.scan(/\d+ example./).empty?
+        number_of_examples = case_example.scan(/\d+/)
+        puts "examples", number_of_examples
+      elsif !case_example.scan(/\d+ failure./).empty?
+        number_of_failures = case_example.scan(/\d+/)
+        puts "failures", number_of_failures
+      elsif !case_example.scan(/\d+ pending/).empty?
+        number_of_pending = case_example.scan(/\d+/)
+        puts "pending", number_of_pending
+      end
+    end
   end
+  
+  def append_spec_data(time, examples, failures, pending, csv_file_location) 
+    saved_current_time = Time.now  
+    CSV.open("#{csv_file_location}","ab") do |csv|
+      csv << ["#{time}", "#{examples}", "#{failures}", "#{pending}"]  
+    end
+  end  
 end
 
- puts test_runtime
-def append_spec_data(time, examples, failures, pending, csv_file_location) 
-  CSV.open("#{csv_file_location}","ab") do |csv|
-    csv << ["#{time}", "#{examples}", "#{failures}", "#{pending}"]  
-  end
-end
