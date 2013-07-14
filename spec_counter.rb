201f029938933f37de3f3ca1e09f30d4d3fcef2e
Mehrzad Far <mehrzadfar@gmail.com>
2013-07-13 20:24:48 -0700

    Changed some variables to instance vars, maybe better

--- spec_counter.rb
+++ spec_counter.rb
@@ -1,5 +1,6 @@
 require 'csv'
-include GitStat
+#include GitStat
+
 module RspecScanner
   def rspec_scanner(file_specified)
     %x( rspec #{file_specified} ).scan(/\d+\.\d+ second.|\d+ example.|\d+ failure.|\d+ pending/)
@@ -16,21 +17,20 @@ module RspecScanner
   def rspec_output_finder(current_array)
     current_array.each do |case_example|
       if !case_example.is_regex_empty?("second")
-        test_runtime = case_example.scan(/\d+\.\d+/)
-        puts "seconds", test_runtime
+        @test_runtime = case_example.scan(/\d+\.\d+/)
+        #puts "seconds", test_runtime
       elsif !case_example.scan(/\d+ example./).empty?
-        number_of_examples = case_example.scan(/\d+/)
-        puts "examples", number_of_examples
+        @number_of_examples = case_example.scan(/\d+/)
+        #puts "examples", number_of_examples
       elsif !case_example.scan(/\d+ failure./).empty?
-        number_of_failures = case_example.scan(/\d+/)
-        puts "failures", number_of_failures
+        @number_of_failures = case_example.scan(/\d+/)
+        #puts "failures", number_of_failures
       elsif !case_example.scan(/\d+ pending/).empty?
-        number_of_pending = case_example.scan(/\d+/)
-        puts "pending", number_of_pending        
+        @number_of_pending = case_example.scan(/\d+/)
+        #puts "pending", number_of_pending        
       end
     end
   end
-  
   def append_spec_data(time, examples, failures, pending, csv_file_location) 
     saved_current_time = Time.now.to_s
     CSV.open("#{csv_file_location}","ab") do |csv|
