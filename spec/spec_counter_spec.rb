748e8fbfb81632466dc438f84a22368cad9e94c4
Mehrzad Far <mehrzadfar@gmail.com>
2013-07-16 14:06:49 -0700

    Begin prototype of run.rb and test

--- spec/spec_counter_spec.rb
+++ spec/spec_counter_spec.rb
@@ -1,10 +1,7 @@
 require 'spec'
-
+require './lib/git_stat'
 describe NumSpecs do
   it 'has the correct values of examples, fails, passes, pending' do
-    examples = 8
-    fails = 4
-    passes = 3
-    pending = 1
+  
   end
 end
