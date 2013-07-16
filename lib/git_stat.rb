1c5ebe69c480a1a8b5ec39d91178dee4707ae46a
Mehrzad Far <mehrzadfar@gmail.com>
2013-07-16 16:36:43 -0700

    More tests done and written

--- lib/git_stat.rb
+++ lib/git_stat.rb
@@ -4,7 +4,7 @@ include GitStat
 
 module RspecScanner  
   def rspec_scanner(file_specified)
-    %x( rspec #{file_specified} ).scan(/\d+\.\d+ second.|\d+ example.|\d+ failure.|\d+ pending/)
+    %x( rspec #{file_specified} ).scan(/\d+\.\d+ second|\d+ example|\d+ failure|\d+ pending/)
   end
   #exec? edit: no need, string interpolation works in rspec_scanner with %x
   
@@ -15,24 +15,34 @@ module RspecScanner
   end
   #spec_run_time = %x( rspec ).scan(/\d+\.\d+ seconds/)
   def rspec_output_finder(current_array)
+    cases_array = []
+    test_runtime = 0.0
+    number_of_examples = 0
+    number_of_failures = 0
+    number_of_pending = 0
+    puts current_array
     current_array.each do |case_example|
       if !case_example.is_regex_empty?("\\d+ second")
-        @test_runtime = case_example.scan(/\d+\.\d+/)[0].to_f
-        puts "seconds", @test_runtime
+        test_runtime = case_example.scan(/\d+\.\d+/)[0].to_f
+        #puts "seconds", test_runtime
       elsif !case_example.is_regex_empty?("\\d+ example")
-        @number_of_examples = case_example.scan(/\d+/)[0].to_i
-        puts "examples", @number_of_examples
+        number_of_examples = case_example.scan(/\d+/)[0].to_i
+        #puts "examples", number_of_examples
       elsif !case_example.is_regex_empty?("\\d+ failure")
-        @number_of_failures = case_example.scan(/\d+/)[0].to_i
-        puts "failures", @number_of_failures
+        number_of_failures = case_example.scan(/\d+/)[0].to_i
+        #puts "failures", number_of_failures
       elsif !case_example.is_regex_empty?("\\d+ pending")
-        @number_of_pending = case_example.scan(/\d+/)[0].to_i
-        puts "pending", @number_of_pending        
+        number_of_pending = case_example.scan(/\d+/)[0].to_i
+        #puts "pending", number_of_pending        
       elsif case_example.is_regex_empty?("\\d+ pending")
-        @number_of_pending = 0
+        number_of_pending = 0
         #pending is the only one that has the possibility to not appear in the rspec output
       end
+      
     end
+    cases_array << number_of_examples << number_of_failures << number_of_pending
+    return cases_array
+    
   end
   def append_spec_data(time, examples, failures, pending, csv_file_location) 
     saved_current_time = Time.now#.to_s
--- spec/spec_counter_spec.rb
+++ spec/spec_counter_spec.rb
@@ -3,10 +3,13 @@ require_relative './../lib/git_stat'
 include RspecScanner
 
 describe RspecScanner do
-  it 'has the correct values of examples, fails, passes, pending' do
+  it 'has the correct values of num examples, fails, pending' do
     test_spec_examples_array = rspec_scanner('./spec/fixtures/some_passing_no_failing_some_pending.rb')
-    rspec_output_finder(test_spec_examples_array)
-    example_numbers_array = [@number_of_examples, @number_of_failures, @number_of_pending]
-    example_numbers_array.should == ([3, 0, 1])
+    rspec_output_finder(test_spec_examples_array).should == ([3, 0, 1])
+  end
+
+  it 'can still work if the pending is non-existent' do
+    test_spec_examples_array = rspec_scanner('./spec/fixtures/some_passing_some_failing_no_pending.rb')
+    rspec_output_finder(test_spec_examples_array).should == ([3, 1, 0])
   end
 end
