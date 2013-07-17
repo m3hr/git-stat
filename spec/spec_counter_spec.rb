1786f29c3614c3460db61e6e0ee544589c3ecb4a
Mehrzad Far <mehrzadfar@gmail.com>
2013-07-17 16:56:46 -0700

    Start of iteration in spec_counter_spec

--- spec/spec_counter_spec.rb
+++ spec/spec_counter_spec.rb
@@ -3,9 +3,10 @@ require_relative './../lib/git_stat'
 include RspecScanner
 
 describe RspecScanner do
-  it 'has the correct values of num examples, fails, pending' do
-    test_spec_examples_array = rspec_scanner('./spec/fixtures/some_passing_no_failing_some_pending.rb')
+  Dir[File.dirname(__FILE__) + '/fixtures/*.rb'].each do |fixture_test|
+    test_spec_examples_array = rspec_scanner(fixture_test)
     rspec_output_finder(test_spec_examples_array).should == ([3, 0, 1])
+  end
   end
 
   it 'can still work if the pending is non-existent' do
