a44d6cd560cfcccf794b1e89d75dd10182af3efc
Mehrzad Far <mehrzadfar@gmail.com>
2013-07-18 15:54:53 -0700

    Began cases_hash for hash based analysis

--- lib/git_stat.rb
+++ lib/git_stat.rb
@@ -17,33 +17,38 @@ module RspecScanner
 
   #spec_run_time = %x( rspec ).scan(/\d+\.\d+ seconds/)
   def rspec_output_finder(current_array)
-    cases_array = []
     test_runtime = 0.0
     number_of_examples = 0
     number_of_failures = 0
     number_of_pending = 0
+    cases_hash = { :runtime => test_runtime, :examples => number_of_examples, :failures => number_of_failures, :pending => number_of_pending }
+
     puts current_array
     current_array.each do |case_example|
       if !case_example.is_regex_empty?("\\d+ second")
         test_runtime = case_example.scan(/\d+\.\d+/)[0].to_f
+        cases_hash[:runtime] = test_runtime
         #puts "seconds", test_runtime
       elsif !case_example.is_regex_empty?("\\d+ example")
         number_of_examples = case_example.scan(/\d+/)[0].to_i
+        cases_hash[:examples] = number_of_examples
         #puts "examples", number_of_examples
       elsif !case_example.is_regex_empty?("\\d+ failure")
         number_of_failures = case_example.scan(/\d+/)[0].to_i
+        cases_hash[:failures] = number_of_failures
         #puts "failures", number_of_failures
       elsif !case_example.is_regex_empty?("\\d+ pending")
         number_of_pending = case_example.scan(/\d+/)[0].to_i
+        cases_hash[:pending] = number_of_pending
         #puts "pending", number_of_pending
       elsif case_example.is_regex_empty?("\\d+ pending")
         number_of_pending = 0
-        #pending is the only metric that has the possibility to not appear in the rspec output
+        cases_hash[:pending] = number_of_pending            vb
+        # pending is the only metric that has the possibility to not appear in the rspec output
       end
 
     end
-    cases_array << test_runtime << number_of_examples << number_of_failures << number_of_pending
-    return cases_array
+    return cases_hash
 
   end
 
