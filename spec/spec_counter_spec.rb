8f14da708fc0e2054e6648cd451d3e4b38394d17
Mehrzad Far <mehrzadfar@gmail.com>
2013-07-22 16:24:15 -0700

    Rewritten tests passing, still does not test every fixture, but is that worth it

--- spec/spec_counter_spec.rb
+++ spec/spec_counter_spec.rb
@@ -3,11 +3,19 @@ require_relative './../lib/git_stat'
 include RspecScanner
 
 describe RspecScanner do
-  it 'detects the correct hash values' do
-    Dir[File.dirname(__FILE__) + '/fixtures/*.rb'].each do |fixture_test|
-    test_spec_examples_array = rspec_scanner(fixture_test)
-    rspec_output_finder(test_spec_examples_array).should == ({ :runtime => !nil, :examples => !nil, :failures => !nil, :pending => !nil })
-  end
+  #it 'detects the correct hash values' do
+  #  Dir[File.dirname(__FILE__) + '/fixtures/*.rb'].each do |fixture_test|
+  #  test_spec_examples_array = rspec_scanner(fixture_test)
+  #  rspec_output_finder(test_spec_examples_array).should == ({ :runtime => !nil, :examples => !nil, :failures => !nil, :pending => !nil })
+  #
+  #end
+  #end
+
+  it 'returns the correct # of examples, failures, pending' do
+    test_spec_examples_array = rspec_scanner('./spec/fixtures/some_passing_some_failing_some_pending.rb')
+    rspec_output_finder(test_spec_examples_array)[:examples].should == 4
+    rspec_output_finder(test_spec_examples_array)[:failures].should == 1
+    rspec_output_finder(test_spec_examples_array)[:pending].should == 1
   end
    #Hash.has_key? has_value?
   it 'can still work if the pending is non-existent' do
@@ -15,7 +23,5 @@ describe RspecScanner do
     rspec_output_finder(test_spec_examples_array)[:examples].should == 3
     rspec_output_finder(test_spec_examples_array)[:failures].should == 1
     rspec_output_finder(test_spec_examples_array)[:pending].should == 0
-     #== ({ :runtime => !nil, :examples => !nil, :failures => !nil, :pending => !nil })
-
   end
 end
