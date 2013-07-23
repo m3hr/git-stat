4c9e9079b379c6f22473c3fa9eefd36b157eb54f
Riley Guerin <guerin.riley@gmail.com>
2013-07-22 17:30:50 -0700

    cleans up some extra variables

--- lib/git_stat.rb
+++ lib/git_stat.rb
@@ -17,33 +17,29 @@ module RspecScanner
 
   #spec_run_time = %x( rspec ).scan(/\d+\.\d+ seconds/)
   def rspec_output_finder(current_array)
-    test_runtime = 0.0
-    number_of_examples = 0
-    number_of_failures = 0
-    number_of_pending = 0
-    cases_hash = { :runtime => test_runtime, :examples => number_of_examples, :failures => number_of_failures, :pending => number_of_pending }
+    cases_hash = { 
+      :runtime => 0.0, 
+      :examples => 0, 
+      :failures => 0, 
+      :pending => 0 
+    }
 
     puts current_array
     current_array.each do |case_example|
-      if !case_example.is_regex_empty?('\\d+ second')
-        test_runtime = case_example.scan(/\d+\.\d+/)[0].to_f
-        cases_hash[:runtime] = test_runtime
+      if !case_example.is_regex_empty?('\\d+ second')        
+        cases_hash[:runtime] = case_example.scan(/\d+\.\d+/)[0].to_f
         #puts "seconds", test_runtime
       elsif !case_example.is_regex_empty?('\\d+ example')
-        number_of_examples = case_example.scan(/\d+/)[0].to_i
-        cases_hash[:examples] = number_of_examples
+        cases_hash[:examples] = case_example.scan(/\d+/)[0].to_i
         #puts "examples", number_of_examples
       elsif !case_example.is_regex_empty?('\\d+ failure')
-        number_of_failures = case_example.scan(/\d+/)[0].to_i
-        cases_hash[:failures] = number_of_failures
+        cases_hash[:failures] = case_example.scan(/\d+/)[0].to_i
         #puts "failures", number_of_failures
       elsif !case_example.is_regex_empty?('\\d+ pending')
-        number_of_pending = case_example.scan(/\d+/)[0].to_i
-        cases_hash[:pending] = number_of_pending
+        cases_hash[:pending] = case_example.scan(/\d+/)[0].to_i
         #puts "pending", number_of_pending
       elsif case_example.is_regex_empty?('\\d+ pending')
-        number_of_pending = 0
-        cases_hash[:pending] = number_of_pending
+        cases_hash[:pending] = 0
         # pending is the only metric that has the possibility to not appear in the rspec output
       end
 
