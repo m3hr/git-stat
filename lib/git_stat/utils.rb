84ce0f90bd9a0d3faafbe99cd235062817dd5754
Mehrzad Far <mehrzadfar@gmail.com>
2013-07-25 12:02:23 -0700

    Strip newlines from revision hashes

--- lib/git_stat/utils.rb
+++ lib/git_stat/utils.rb
@@ -4,10 +4,12 @@ module GitStat
   end
   def get_first_git_revision(branch)
     # requires git 1.7.4.2+ http://stackoverflow.com/questions/1006775/how-to-reference-the-initial-commit
-    %x( git rev-list --max-parents=0 #{branch} )
+    first_with_newline = %x( git rev-list --max-parents=0 #{branch} )
+    first_revision = first_with_newline.delete("\n")    
   end
 
   def get_latest_git_revision(branch)
-    %x( git rev-parse #{branch} )
+    latest_with_newline = %x( git rev-parse #{branch} )
+    latest_revision = latest_with_newline.delete("\n")
   end
 end
